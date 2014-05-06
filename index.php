35f88f30f6254722267bbed14897f5b19025021a
Bruno <bruno@bruno-pc.(none)>
2014-05-06 12:01:09 -0300

    Ajustes nas classes css

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
       <div class="banner">
         <a href="">
           <img src="images/img1.jpg" alt="Imagem 2">
-          <span class="legend">Descrição da imagem 1</span>
+          <span class="description">Descrição da imagem 1</span>
         </a>
       </div>     
       <div class="banner">
         <a href="">
           <img src="images/img2.jpg" alt="Imagem 1">
-          <span class="legend">Descrição da imagem 2</span>
+          <span class="description">Descrição da imagem 2</span>
         </a>
       </div>               
     </div>
